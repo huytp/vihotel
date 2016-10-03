2463c996942a6ce050004646a8c261d0590b1cdd
nusnick <nick@nustechnology.com>
2016-10-03 17:18:36 +0700

    complete i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   mount Ckeditor::Engine => '/ckeditor'
   devise_for :users
   get "contact", to: "contacts#index", as: "contact_index"
@@ -44,6 +45,7 @@ Rails.application.routes.draw do
   end
 
   scope '/admin' do
+    resources :currency, only: [:index, :update]
     resources :errors, only: [] do
       collection do
         get "", to: "errors#error", as: "error"
