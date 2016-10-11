e6fe2e3ad7a8bf764e9ce32d656c7f2beed71b75
nusnick <nick@nustechnology.com>
2016-10-12 00:33:35 +0700

    Add chatbox

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -2,7 +2,6 @@
   $(document).on("ready", function() {
     init();
   });
-
   var init = function() {
     slider();
     slideHome();
@@ -21,7 +20,20 @@
     }
     DatePicker();
     rollTop();
+    chatBox();
   };
+
+  var chatBox = function() {
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/57fd179f9ca1830bdc904e05/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+  }
 
   var rollTop = function() {
     $(".backtop").click(function() {
