b67daae93813f89662669744ef51d0831055477f
nusnick <nick@nustechnology.com>
2016-10-12 17:59:43 +0700

    Add facebook social

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -21,8 +21,17 @@
     DatePicker();
     rollTop();
     chatBox();
+    likeFacebook();
   };
-
+  var likeFacebook = function() {
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=992014527588145";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }
   var chatBox = function() {
     var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
     (function(){
