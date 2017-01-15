85a5b7ef869e493cb8370afd935ed1f790e6ebf0
sagodevnick <nick@sagodev.com>
2017-01-15 21:42:55 +0700

    Done

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,29 @@
 class HomesController < ApplicationController
 
   def index
-
+    @hotel_overview = HotelOverview.where(post_type: 'hotel-overview').last
+    @photo_overviews = @hotel_overview.photo_overviews
+    @restaurant = HotelOverview.where(post_type: 'restaurant').last
+    if @restaurant.present?
+      @photo_restaurants = @restaurant.photo_overviews
+    end
+    @coffee = HotelOverview.where(post_type: 'bar-and-coffee').last
+    if @coffee.present?
+      @photo_coffees = @coffee.photo_overviews
+    end
+    @spa = HotelOverview.where(post_type: 'spa-and-massage').last
+    if @spa.present?
+      @photo_spas = @spa.photo_overviews
+    end
+    @pool = HotelOverview.where(post_type: 'pool').last
+    if @pool.present?
+      @photo_pools = @pool.photo_overviews
+    end
+    @premier_collections = ParentRoomType.where(name: "Premier Collection").last.room_types
+    @smart_collections = ParentRoomType.where(name: "Smart Superior Collection").last.room_types
+    @deluxe_collections = ParentRoomType.where(name: "Deluxe Collection").last.room_types
+    @currency = Currency.last.currency
+    @slogan = Slogan.last.content_slogan
   end
 
 
